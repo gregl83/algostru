baf1841e67e0fd966d2189e04e7100b38c40b85e
Gregory Langlais <general@gregorylanglais.com>
2020-12-31 00:45:43 -0500

    add merge sort docs

--- src/collections/sort/merge_sort.rs
+++ src/collections/sort/merge_sort.rs
@@ -1,3 +1,18 @@
+// Merge Sort
+//
+// Input: vector x of n elements
+// Output: sorted vector of n elements
+//
+// =================================================================================================
+//
+// if x length equals 0 or 1 then
+//     base case: return vector
+//
+//     recursive sort:
+//     a = merge sort of first halve of x
+//     b = merge sort of second halve of x
+//
+//     return result: x sorted
 pub fn sort<T>(x: Vec<T>) -> Vec<T> {
     x
 }
