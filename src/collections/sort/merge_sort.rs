de7ce131655b7516ccd529e78e8437eeb5a083ec
Gregory Langlais <general@gregorylanglais.com>
2020-12-31 01:08:03 -0500

    add merge sort fns

--- src/collections/sort/merge_sort.rs
+++ src/collections/sort/merge_sort.rs
@@ -1,3 +1,25 @@
+// Merge Vectors
+//
+// Input: two sorted vectors x and y
+// Output: merged vector containing the elements of x and y
+//
+// =================================================================================================
+//
+// result = new vector
+// x_index = 0
+// y_index = 0
+//
+// while index_x < length of x and index_y < length of y
+//     result.push smaller of x[x_index] and y[y_index]
+//     increment index for smaller of x[x_index] and y[y_index] respectively
+//
+// result.push for all elements remaining in x or y
+//
+// return result
+fn merge(x: Vec<isize>, y: Vec<isize>) -> Vec<isize> {
+
+}
+
 // Merge Sort
 //
 // Input: vector x of n elements
@@ -13,6 +35,17 @@
 //     b = merge sort of second halve of x
 //
 //     return result: x sorted
-pub fn sort<T>(x: Vec<T>) -> Vec<T> {
-    x
+pub fn sort(x: Vec<isize>) -> Vec<isize> {
+    if x.len() <= 1 {
+        return x;
+    }
+
+    let midpoint = x.len() / 2;
+    let a = x[midpoint..];
+    let b = x[..midpoint];
+
+    let c = merge_sort(a.to_vec());
+    let d = merge_sort(b.to_vec());
+
+    merge(c, d)
 }
