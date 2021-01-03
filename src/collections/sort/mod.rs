db4122cd5f14818e08e607d5e22fe1399b6a2384
Gregory Langlais <general@gregorylanglais.com>
2021-01-02 22:37:36 -0500

    add quick sort test fail

--- src/collections/sort/mod.rs
+++ src/collections/sort/mod.rs
@@ -1,4 +1,5 @@
 pub mod merge_sort;
+pub mod quick_sort;
 
 #[cfg(test)]
 mod tests {
@@ -14,6 +15,20 @@ mod tests {
         ];
 
         let sorted = merge_sort::sort(x);
+
+        assert_eq!(sorted, expectation);
+    }
+
+    #[test]
+    fn test_quick_sort() {
+        let x: Vec<isize> = vec![
+            10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10
+        ];
+        let expectation: Vec<isize> = vec![
+            -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ];
+
+        let sorted = quick_sort::sort(x);
 
         assert_eq!(sorted, expectation);
     }
---
+++ src/collections/sort/quick_sort.rs
@@ -0,0 +1,11 @@
+// Quick Sort
+//
+// Input: vector x of n elements
+// Output: sorted vector of n elements
+//
+// =================================================================================================
+//
+// todo
+pub fn sort(x: Vec<isize>) -> Vec<isize> {
+    x
+}
