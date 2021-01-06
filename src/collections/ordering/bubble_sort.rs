38b3185de1d714974103f159c623e358b27503f6
Gregory Langlais <general@gregorylanglais.com>
2021-01-06 12:59:10 -0500

    update bubble sort completed

--- src/collections/ordering/bubble_sort.rs
+++ src/collections/ordering/bubble_sort.rs
@@ -1,11 +1,22 @@
 // Bubble Sort
 //
-// Input: vector x of n elements
+// Input: mutable reference of vector x of n elements
 // Output: sorted vector of n elements
 //
 // =================================================================================================
 //
-// todo
-pub fn sort(x: Vec<isize>) -> Vec<isize> {
-    x
+// loop n elements in x setting index to i
+//     loop n elements in x starting from index i setting index to j
+//         if index j element is greater than index j + 1 element perform swap
+//
+pub fn sort(x: &mut Vec<isize>) {
+    let last_index = x.len() - 1;
+    for i in 0..last_index {
+        for j in 0..last_index - i {
+            let next = j + 1;
+            if x[j] > x[next] {
+                x.swap(j, next);
+            }
+        }
+    }
 }
--- src/collections/ordering/mod.rs
+++ src/collections/ordering/mod.rs
@@ -9,16 +9,16 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        let x: Vec<isize> = vec![
+        let mut x: Vec<isize> = vec![
             10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10
         ];
-        let expectation: Vec<isize> = vec![
+        let mut expectation: Vec<isize> = vec![
             -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
         ];
 
-        let sorted = bubble_sort::sort(x);
+        bubble_sort::sort(&mut x);
 
-        assert_eq!(sorted, expectation);
+        assert_eq!(x, expectation);
     }
 
     #[test]
