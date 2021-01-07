43b07563a33a64bea9c3379b0447e3e390b899f6
Gregory Langlais <general@gregorylanglais.com>
2021-01-07 00:08:05 -0500

    add functions for strassen multiply

--- src/collections/multiplication/strassen.rs
+++ src/collections/multiplication/strassen.rs
@@ -1,10 +1,32 @@
-use num_bigint::BigInt;
+type Matrix = Vec<Vec<isize>>;
 
-fn strassen(x: BigInt, y: BigInt) -> BigInt {
+// Quarter Matrix
+//
+// Input: n-vector vector x
+// Output: tuple of vector x quartered
+// Assumption: x is square matrix
+//
+// =================================================================================================
+//
+// todo
+fn quarter_matrix(x: Matrix) -> (Matrix, Matrix, Matrix, Matrix) {
+    
+}
+
+// Strassen Matrix Multiplication
+//
+// Input: n-vector vectors x and y
+// Output: n-vector vector product of x and y
+// Assumption: x and y are equal squares
+//
+// =================================================================================================
+//
+// todo
+fn strassen(x: Matrix, y: Matrix) -> Matrix {
     // fixme - implement strassen
     x
 }
 
-pub fn multiply(x: BigInt, y: BigInt) -> BigInt {
+pub fn multiply(x: Matrix, y: Matrix) -> Matrix {
     strassen(x, y)
 }
