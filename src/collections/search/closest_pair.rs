4e7503b00e7444b766898a2b0da555982d984339
Gregory Langlais <general@gregorylanglais.com>
2021-01-08 18:51:54 -0500

    add closest pair func placeholder

--- src/collections/search/closest_pair.rs
+++ src/collections/search/closest_pair.rs
@@ -1,3 +1,26 @@
-fn closest_pair() {}
+type Point = (isize, isize);
+type Plane = Vec<Point>;
 
-pub fn find() {}
+// Closest Pair
+//
+// Input: Plane x of n Point elements
+// Output: pair of Point elements from x that are closest
+//
+// =================================================================================================
+//
+// todo
+fn closest_pair(x: Plane) -> (Point, Point) {
+    (x[0].0, x[0].1)
+}
+
+// Find Closest Pair
+//
+// Input: Plane x of n Point elements
+// Output: pair of Point elements from x that are closest
+//
+// =================================================================================================
+//
+// todo
+pub fn find(x: Plane) -> (Point, Point) {
+    closest_pair(x)
+}
