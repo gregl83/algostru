5960cfcafec0043f90926f581668ec88313f4c5b
Gregory Langlais <general@gregorylanglais.com>
2021-01-09 19:22:46 -0500

    add merge sort for plane of pairs

--- src/collections/search/closest_pair.rs
+++ src/collections/search/closest_pair.rs
@@ -1,5 +1,53 @@
+enum Axis { X, Y }
 type Point = (isize, isize);
 type Plane = Vec<Point>;
+
+fn get_point_axis(point: Point, axis: &Axis) -> isize {
+    match axis {
+        Axis::X => point.0,
+        Axis::Y => point.1,
+    }
+}
+
+fn merge_pairs(a: Plane, b: Plane, axis: &Axis) -> Plane {
+    let mut merged: Plane = Vec::new();
+    let mut a_index = 0;
+    let mut b_index = 0;
+
+    while a_index < a.len() && b_index < b.len() {
+        if get_point_axis(a[a_index], &axis) <= get_point_axis(b[b_index], &axis) {
+            merged.push(a[a_index]);
+            a_index += 1;
+        } else {
+            merged.push(b[b_index]);
+            b_index += 1;
+        }
+    }
+
+    for element in a[a_index..].to_vec() {
+        merged.push(element);
+    }
+    for element in b[b_index..].to_vec() {
+        merged.push(element);
+    }
+
+    merged
+}
+
+fn sort_pairs(x: Plane, axis: &Axis) -> Plane {
+    if x.len() <= 1 {
+        return x;
+    }
+
+    let midpoint = x.len() / 2;
+    let a = &x[..midpoint];
+    let b = &x[midpoint..];
+
+    let c = sort_pairs(a.to_vec(), &axis);
+    let d = sort_pairs(b.to_vec(), &axis);
+
+    merge_pairs(c, d, &axis)
+}
 
 // Closest Pair
 //
@@ -23,4 +71,59 @@ fn closest_pair(x: Plane) -> (Point, Point) {
 // todo
 pub fn find(x: Plane) -> (Point, Point) {
     closest_pair(x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_pairs() {
+        let a: Plane = vec![
+            (0, 1),
+            (1, 2)
+        ];
+        let b: Plane = vec![
+            (2, 3),
+            (3, 4),
+            (4, 5)
+        ];
+        let axis = Axis::X;
+
+        let expectation: Plane = vec![
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5)
+        ];
+
+        let result = merge_pairs(a, b, &axis);
+
+        assert_eq!(result, expectation);
+    }
+
+    #[test]
+    fn test_sort_pairs_x() {
+        let pairs: Plane = vec![
+            (4, 5),
+            (3, 4),
+            (2, 3),
+            (1, 2),
+            (0, 1)
+        ];
+        let axis = Axis::X;
+
+        let expectation: Plane = vec![
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5)
+        ];
+
+        let result = sort_pairs(pairs, &axis);
+
+        assert_eq!(result, expectation);
+    }
 }
