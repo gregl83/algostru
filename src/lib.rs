34a54c3630df90f88b4e28e9b638635ce99e1c48
Gregory Langlais <general@gregorylanglais.com>
2021-01-16 14:14:05 -0500

    add crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 pub mod arithmetic;
 pub mod collections;
+
+//! Algostru is a library of algorithms (algo) and data structures (stru) demonstrating
+//! asymptotic notation without granular scrutiny on operation specific performance.
+//!
+//! Included is a command line interface tool that *might* grow into toolset to aid with algorithm
+//! and data structure implementations. For now, it renders a Big-O cheatsheet in your terminal.
