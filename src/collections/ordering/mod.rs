d498094714743e8f7faba88b9d17050c253ec594
Gregory Langlais <general@gregorylanglais.com>
2021-01-18 09:53:00 -0500

    update time sort test to ignored until complete

--- src/collections/ordering/mod.rs
+++ src/collections/ordering/mod.rs
@@ -4,6 +4,7 @@ pub mod merge_sort;
 pub mod merge_sort_inversion_count;
 pub mod quick_sort;
 pub mod selection_sort;
+pub mod time_sort;
 
 #[cfg(test)]
 mod tests {
@@ -97,6 +98,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_time_sort() {
         let mut x: Vec<isize> = vec![
             10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10
