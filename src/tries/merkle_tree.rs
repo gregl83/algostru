deddffbcd676bff299f9f1597707982380b84198
Gregory Langlais <general@gregorylanglais.com>
2021-02-01 15:34:09 -0500

    add merkle base

--- src/tries/merkle_tree.rs
+++ src/tries/merkle_tree.rs
@@ -1,3 +1,22 @@
-struct Merkle {
+struct Node {
     // todo
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Node {
+
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tree() {
+        // todo
+        let mut tree = Node::new();
+    }
 }
